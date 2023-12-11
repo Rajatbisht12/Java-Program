d392c7b4e86f262644012a21ce867d5464951f6e
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2023-12-11 08:01:35 +0000

    Program

---
+++ Program8.java
@@ -0,0 +1,13 @@
+public class Program8 {
+    public static void main(String[] args) {
+        String s ="Edureka";
+        String s1 = "Hello";
+//        System.out.println(s.length());
+//        System.out.println(s.substring(2));
+//        System.out.println(s.compareTo(s1));
+//        System.out.println(s.isEmpty());
+//        System.out.println(s.toUpperCase());
+//        System.out.println(s.toLowerCase());
+        System.out.println(s.valueOf(s1));
+    }
+}
