ab9ff13c57abcb1e95c589074e9867d5ab09e8ea
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2023-12-11 08:00:23 +0000

    Program

---
+++ Program7.java
@@ -0,0 +1,12 @@
+public class Program7 {
+    public static void main(String[] args) {
+        int [][] arrayOfIntegers = new int[5][5];
+        int [][] a = { {1, 2, 3, 4, 50}, {2, 56, 77, 8, 6} };
+        for(int i = 0; i<5; i++){
+            for(int j = 0; j<5; j++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
