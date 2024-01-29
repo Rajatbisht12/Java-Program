2ecfb57edf3ebc686f8f03bc1dabc7ccfacce794
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2024-01-29 19:48:33 +0000

    array

--- ArrayandArratList/Arrays.java
+++ ArrayandArratList/Arrays.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 class Arrays{
     public static void main(String[] args){
         int [] rnos = {23, 12, 45, 32, 25};
         for(Integer arr : rnos){
             System.out.println(arr + " ");
         }
+
+        // User Input
+
+        Scanner sc = new Scanner(System.in);
+        int [] arr = new int[6];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        //Printing the array
+        for(Integer ar : arr){
+            System.out.println(ar);
+        }
+        sc.close();
     }
 }
