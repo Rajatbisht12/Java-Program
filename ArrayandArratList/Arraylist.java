a7d8d5e09fde1b59d63c2d682f1f36cf21f3f8a4
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2024-02-01 20:18:58 +0000

    arraylist

--- ArrayandArratList/Arraylist.java
+++ ArrayandArratList/Arraylist.java
@@ -1,11 +1,26 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Arraylist {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<Integer>(10);
-        list.add(14);
-        list.add(15);
-        list.add(11);
-        System.out.println(list);
+        // list.add(14);
+        // list.add(15);
+        // list.add(11);
+        // System.out.println(list);
+
+        // User Input
+
+        for(int i = 0; i < 5; i++){
+            list.add(sc.nextInt());
+        }
+        
+        //Printing the ArrayList
+        for(Integer arr : list){
+            System.out.println(arr);
+        }
+
+        sc.close();
     }
 }
