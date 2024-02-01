a7bab5194642fcbd83c7dbe6b614c461edc87c80
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2024-02-01 20:16:02 +0000

    arraylist

---
+++ ArrayandArratList/Arraylist.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>(10);
+        list.add(14);
+        list.add(15);
+        list.add(11);
+        System.out.println(list);
+    }
+}
