299b76efea006f7fa499bc5178e6bb366f1ebf3e
Rajatbisht12 <rjatgithub123@gmail.com>
2024-02-15 00:39:21 +0530

    commit

---
+++ Patterns/Second.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class Second {
+    public static void main(String [] args){
+		pattern2(4);
+	}
+	static void pattern2(int n){
+		for(int row = 1; row <= n; row++){
+			for(int col = 1; col <= n; col++){
+				System.out.print("*");
+			}
+			// when one row is printed we need to add a newline
+			System.out.println();
+		}
+	}
+}
--- notes/Patterns.md
+++ notes/Patterns.md
@@ -25,7 +25,7 @@ class Solution{
 		pattern1(4);
 	}
 	static void pattern1(int n){
-		for(int row = 1; row < n; row++){
+		for(int row = 1; row <= n; row++){
 			for(int col = 1; col <= row; col++){
 				System.out.print("*");
 			}
