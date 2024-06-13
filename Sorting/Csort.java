a12998919b473a49476e1ded8f02b9477a7a3ef6
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2024-06-13 18:53:33 +0000

    Cycle sort

---
+++ Sorting/Csort.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+public class Csort {
+    public static void main(String[] args) {
+        int [] arr = {5, 4, 2, 3, 1};
+        sort(arr);
+        for(int ar: arr){
+            System.out.println(ar);
+        }
+    }
+
+    static void sort(int [] arr){
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }else{
+                i += 1;
+            }
+        }
+    }
+
+    static void swap(int [] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
