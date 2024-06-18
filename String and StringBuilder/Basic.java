21a705869b52164686abf4bd3cc6ff08b8a22ee9
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2024-06-18 08:50:51 +0000

    string

---
+++ String and StringBuilder/Basic.java
@@ -0,0 +1,7 @@
+public class Basic {
+    public static void main(String[] args) {
+        String var = "Hello";
+        System.out.println(var + 3);
+        System.out.println(var + 'c');
+    }
+}
