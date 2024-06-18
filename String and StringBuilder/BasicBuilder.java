02c842843c37fba1247302439140f3507201feaa
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2024-06-18 11:35:26 +0000

    string

---
+++ String and StringBuilder/BasicBuilder.java
@@ -0,0 +1,10 @@
+public class BasicBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 26; i++){
+            char ch = (char)('a' + i);
+            sb.append(ch);
+        }
+        System.out.println(sb.toString());
+    }
+}
