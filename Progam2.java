8f0e813422d499c0d5f11674134c604fa0537631
Rajatbisht12 <rjatgithub123@gmail.com>
2024-06-25 21:04:05 +0530

    Changes

--- Progam2.java
+++ Progam2.java
@@ -1,7 +1,7 @@
-public class Progam2 {
-    public static void main(String[] args) {
-        Program1 obj = new Program1();
-        obj.Test2();
-        System.out.println("Hell");
-    }
-}
+// public class Progam2 {
+//     public static void main(String[] args) {
+//         Program1 obj = new Program1();
+//         obj.Test2();
+//         System.out.println("Hell");
+//     }
+// }
