671d7e56b158fdd416073c59c357e3b8e0871c34
Rajatbisht12 <rjatgithub123@gmail.com>
2024-06-30 00:12:12 +0530

    PowerOf a with bitwise operation

---
+++ MathInjava/Power.java
@@ -0,0 +1,17 @@
+package MathInjava;
+
+public class Power {
+    public static void main(String[] args) {
+        int base = 3;
+        int power = 63;
+        int ans = 1;
+        while (power > 0) {
+            if((power & 1) == 1){
+                ans *= base;
+            }
+            base *= base;
+            power = power >> 1;
+        }
+        System.out.println(ans);
+    }
+}
