510e1a03c399ec07e9b2d493471582f1b8259a04
Rajatbisht12 <rjatgithub123@gmail.com>
2024-07-02 15:25:21 +0530

    find all occurence

---
+++ Recursion/FindAllOccurence.java
@@ -0,0 +1,29 @@
+package Recursion;
+
+import java.util.ArrayList;
+
+public class FindAllOccurence {
+    
+    static ArrayList<Integer> list = new ArrayList<>();
+    public static void findAllIndex(int [] arr, int target, int index){
+        if(index == arr.length){
+            return;
+        }
+
+        if(arr[index] == target){
+            list.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
+    }
+
+    // public static ArrayList findAllIndex(int [] arr, int target, int index, ArrayList<Integer> list){
+    //     if(index == arr.length){
+    //         return list;
+    //     }
+
+    //     if(arr[index] == target){
+    //         list.add(index);
+    //     }
+    //     findAllIndex(arr, target, index + 1, list);
+    // }
+}
