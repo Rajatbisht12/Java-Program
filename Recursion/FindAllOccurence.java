ec3bd741b0f8508a21a4c92a9bd81bc1d041d2be
Rajatbisht12 <rjatgithub123@gmail.com>
2024-07-02 16:19:30 +0530

    changes

--- Recursion/FindAllOccurence.java
+++ Recursion/FindAllOccurence.java
@@ -27,4 +27,20 @@ public class FindAllOccurence {
         return findAllIndex(arr, target, index + 1, list);
     }
 
+    public static ArrayList<Integer> findAllIndex2(int [] arr, int target, int index){
+        ArrayList<Integer> list  = new ArrayList<>();
+        if(index == arr.length){
+            return list;
+        }
+
+        // this will contain answer for that function call only
+        if(arr[index] == target){
+            list.add(index);
+        }
+        ArrayList<Integer> ansFromBelowCalls = findAllIndex2(arr, target, index+1);
+
+
+        list.addAll(ansFromBelowCalls);
+        return list;
+    }
 }
