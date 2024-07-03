1d87e7dd31e208b4dd287226b1fc51487b8ace36
Rajatbisht12 <rjatgithub123@gmail.com>
2024-07-03 13:25:40 +0530

    rotated binary search

---
+++ Recursion/RotatedBinarySearch.java
@@ -0,0 +1,30 @@
+package Recursion;
+
+class RotatedBinarySearch{
+    public static void main(String[] args) {
+        int [] arr = {1, 2, 3, 4, 5, 6, 10};
+        System.out.println(search(arr, 10, 0, arr.length));
+    }
+
+    static int search(int [] arr, int target, int s, int e){
+        if(s > e){
+            return -1;
+        }
+
+        int m = s + (e - s) / 2;
+        if(arr[m] == target){
+            return m;
+        }
+        if(arr[s] <= arr[m]){
+            if(target >= arr[s] && target <= arr[m]){
+                return search(arr, target, s, m-1);
+            }else{
+                return search(arr, target, m+1, e);
+            }
+        }
+        if(target >= arr[m] && target <= arr[e]){
+            return search(arr, target, m+1, e);
+        }
+        return search(arr, target, s, m-1);
+    }
+}
