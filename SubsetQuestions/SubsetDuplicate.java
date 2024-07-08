b9bd4bb0d4cc1f100b998e691210f95d89b72e86
Rajatbisht12 <rjatgithub123@gmail.com>
2024-07-08 23:28:44 +0530

    duplicate

---
+++ SubsetQuestions/SubsetDuplicate.java
@@ -0,0 +1,35 @@
+package SubsetQuestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2};
+        System.out.println(subsetDuplicate(arr));
+    }
+
+    static List<List<Integer>> subsetDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}
