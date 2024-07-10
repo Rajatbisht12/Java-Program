c58f889fd7fc0a62bcb8e0adda989585d0727b64
Rajatbisht12 <rjatgithub123@gmail.com>
2024-07-10 22:35:12 +0530

    changes

--- SubsetQuestions/Phonepad.java
+++ SubsetQuestions/Phonepad.java
@@ -1,8 +1,10 @@
 package SubsetQuestions;
 
+import java.util.ArrayList;
+
 public class Phonepad {
     public static void main(String[] args) {
-        pad("", "12");
+        System.out.println(padArray("", "12"));
     }
 
     static void pad(String p, String up){
@@ -16,5 +18,21 @@ public class Phonepad {
             char ch = (char)('a' + i);
             pad(p+ch, up.substring(1));
         }
+    }
+
+    static ArrayList<String> padArray(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        int digit = up.charAt(0) - '0';
+        ArrayList<String> ans = new ArrayList<>();
+        for(int i = (digit - 1) * 3; i < digit * 3; i++){
+            char ch = (char)('a' + i);
+            ans.addAll(padArray(p+ch, up.substring(1)));
+        }
+        return ans;
     }
 }
