b7d668636b3488cab79781c723acaacb18bc6f43
Rajatbisht12 <rjatgithub123@gmail.com>
2024-07-11 23:50:04 +0530

    List of array

--- SubsetQuestions/Dice.java
+++ SubsetQuestions/Dice.java
@@ -1,12 +1,14 @@
 package SubsetQuestions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Dice {
     public static void main(String[] args) {
         // dice("", 4);
 
-        System.out.println(diceArray("", 4));
+        // System.out.println(diceArray("", 4));
+        System.out.println(diceArraySUb(0, 4));
     }
 
     static void dice(String p, int target){
@@ -29,6 +31,24 @@ public class Dice {
         ArrayList<String> ans = new ArrayList<>();
         for(int i = 1; i <= 6 && i <= target; i++){
             ans.addAll(diceArray(p+i, target-i));
+        }
+        return ans;
+    }
+
+    static List<List<Integer>> diceArraySUb(Integer p, int target){
+        if(target == 0){
+            List<List<Integer>> list = new ArrayList<>();
+            list.add(new ArrayList<>());
+            return list;
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 1; i <= 6 && i <= target; i++){
+            List<List<Integer>> subResults = diceArraySUb(p + i, target - i);
+            for(List<Integer> subResult : subResults){
+                List<Integer> newCombinaation = new ArrayList<>(subResult);
+                newCombinaation.add(i);
+                ans.add(newCombinaation);
+            }
         }
         return ans;
     }
