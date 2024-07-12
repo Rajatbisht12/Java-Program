f58f13ede0250d170cccc360668f5d4fa98df7a4
Rajatbisht12 <rjatgithub123@gmail.com>
2024-07-13 01:01:43 +0530

    update

--- BackTracking/MazeWithObstacle.java
+++ BackTracking/MazeWithObstacle.java
@@ -4,11 +4,13 @@ public class MazeWithObstacle {
     public static void main(String[] args) {
         boolean [][] maze = {
             {true, true, true},
-            {true, false, true},
+            {true, true, true},
             {true, true, true}
         };
 
-        pathRestriction("", maze, 0, 0);
+        // pathRestriction("", maze, 0, 0);
+
+        allPaths("", maze, 0, 0);
     }
 
     static void pathRestriction(String p, boolean [][] maze, int r, int c){
@@ -28,5 +30,36 @@ public class MazeWithObstacle {
         if(c < maze[0].length -1){
             pathRestriction(p+'R', maze, r, c+1);
         }
+    }
+
+    static void allPaths(String p, boolean [][] maze, int r, int c){
+        if(r==maze.length-1 && c==maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        maze[r][c] = false;
+
+        if(r < maze.length-1){
+            allPaths(p+'D', maze, r+1, c);
+        }
+
+        if(c < maze[0].length -1){
+            allPaths(p+'R', maze, r, c+1);
+        }
+
+        if(r > 0){
+            allPaths(p+'U', maze, r-1, c);
+        }
+
+        if(c > 0){
+            allPaths(p+'L', maze, r, c-1);
+        }
+
+        maze[r][c] = true;
     }
 }
