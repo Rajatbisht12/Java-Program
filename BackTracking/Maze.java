a89dfb86cbe8991cbd5c39fd9ac519be550d18f9
Rajatbisht12 <rjatgithub123@gmail.com>
2024-07-13 00:22:39 +0530

    Allpaths

--- BackTracking/Maze.java
+++ BackTracking/Maze.java
@@ -2,7 +2,9 @@ package BackTracking;
 
 public class Maze {
     public static void main(String[] args) {
-        System.out.println(count(3, 3));
+        // System.out.println(count(3, 3));
+
+        allPath("", 3, 3);
     }
 
     static int count(int r, int c){
@@ -14,5 +16,22 @@ public class Maze {
         int right = count(r, c-1);
 
         return left + right;
+    }
+
+    static void allPath(String p, int r, int c){
+        if(r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+
+        if(r > 1){
+            allPath(p+'V', r-1, c);
+        }
+        if(c>1){
+            allPath(p+'H', r, c-1);
+        }
+        if(r>1 && c>1){
+            allPath(p+'D', r-1, c-1);
+        }
     }
 }
